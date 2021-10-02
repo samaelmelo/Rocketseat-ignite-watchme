1467520b14e7d3f49365edc293275a3b8bcb7b0d
Samael Melo <samaelmelo.dev@gmail.com>
2021-10-01 21:39:27 -0400

    refatoração do componente App, Content e Sidebar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
-  // Complete aqui
+import React from "react";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss"
+
+export function Content(props) {
+  return(
+    <div className="container">
+      <header>
+          <span className="category">Categoria: <span>
+          {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard 
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+      </main>
+    </div>
+  )
 }
+
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,22 @@
-export function SideBar() {
-  // Complete aqui
+import React from "react"
+import { Button } from "./Button"
+
+import "../styles/sidebar.scss"
+
+export function SideBar(props) {
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={()=>props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}/>
+        ))}
+        </div>
+    </nav>
+  )
 }
